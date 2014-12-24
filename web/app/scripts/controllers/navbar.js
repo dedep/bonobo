5e6f8d2af3890c5a7fff953da9d8dbc4431d4bff
Dawid Adamek <dawid.adamek@javart.eu>
2014-12-24 23:40:15 +0100

    Keyboard shortcuts navigation enabled.

--- web/app/scripts/controllers/navbar.js
+++ web/app/scripts/controllers/navbar.js
@@ -1,4 +1,22 @@
 angular.module('webApp')
-    .controller('NavBarCtrl', function ($scope) {
+    .controller('NavBarCtrl', function ($scope, $location, hotkeys) {
+        hotkeys.bindTo($scope)
+            .add({
+                combo: 'alt+a',
+                description: 'About page',
+                callback: function() {
+                    $location.path('/about');
+                }
+            })
+            .add({
+                combo: 'alt+h',
+                description: 'Home page',
+                callback: function() {
+                    $location.path('/');
+                }
+            });
 
+        $scope.toggleHelp = function() {
+            hotkeys.toggleCheatSheet();
+        };
     });
