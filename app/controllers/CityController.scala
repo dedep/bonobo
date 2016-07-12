38d305c1f9be716ac2bb9e32b7e8fe03b9254fc5
Dawid Adamek <adamdawidek@gmail.com>
2016-07-13 00:01:11 +0200

    Removing generic controllers.

--- app/controllers/CityController.scala
+++ app/controllers/CityController.scala
@@ -1,15 +1,17 @@
 package controllers
 
-import controllers.validator.{CityValidator, BaseCrudValidator}
-import db.dao.{CityDao, BaseCrudDao}
-import dto.mapper.{CityDtoMapper, BaseDtoMapper}
+import controllers.validator.{BaseCrudValidator, CityValidator}
+import db.dao.{BaseCrudDao, CityDao}
+import dto.mapper.{BaseDtoMapper, CityDtoMapper}
+import models.Common._
 import models.territory.City
+import play.api.libs.json.{JsArray, Json}
 import scaldi.{Injectable, Injector}
 
-class CityController(implicit inj: Injector) extends BaseCrudController[City] with Injectable {
-  override protected val validator: BaseCrudValidator[City] = inject[CityValidator]
-  override protected val dao: BaseCrudDao[City] = inject[CityDao]
-  override protected val dto: BaseDtoMapper[City] = inject[CityDtoMapper]
+class CityController(implicit inj: Injector) extends BaseController with Injectable {
+  protected val validator: BaseCrudValidator[City] = inject[CityValidator]
+  protected val dao: BaseCrudDao[City] = inject[CityDao]
+  protected val dto: BaseDtoMapper[City] = inject[CityDtoMapper]
 
   def findCity(id: Long) = serveHttpResponseWithDB {
     implicit rs => {
@@ -18,5 +20,64 @@ class CityController(implicit inj: Injector) extends BaseCrudController[City] wi
         case Some(c: City) => Ok(views.html.city(c))
       }
     }
+  }
+
+  def create(routeIds: Id*) = serveHttpResponseWithTransactionalDB {
+    implicit rs => {
+      dto.form.bindFromRequest.fold(
+        hasErrors => {
+          BadRequest("Cannot bind model from request " + hasErrors)
+        },
+        success => {
+          val obj = success.toObject
+          validator.validateCreateRequest(obj)(rs.dbSession)
+          val id = dao.insert(obj)(rs.dbSession)
+
+          Ok(Json.obj("id" -> id))
+        }
+      )
+    }
+  }
+
+  def delete(routeIds: Id*) = serveHttpResponseWithTransactionalDB {
+    implicit rs => {
+      dao.find(routeIds)(rs.dbSession).map(obj => {
+        validator.validateDeleteRequest(obj)(rs.dbSession)
+        dao.delete(obj)(rs.dbSession)
+        Ok(s"Entity [id=${routeIds.last}] has been deleted.")
+      }).getOrElse(BadRequest(s"Cannot delete non-existent entity: [id=${routeIds.last}]"))
+    }
+  }
+
+  def edit(routeIds: Id*) = serveHttpResponseWithTransactionalDB {
+    implicit rs => {
+      dao.find(routeIds)(rs.dbSession).map(obj => {
+        dto.form.bindFromRequest.fold(
+          hasErrors => {
+            BadRequest(s"Cannot bind entity from request $hasErrors")
+          },
+          success => {
+            validator.validateEditRequest(obj, success.toObject)(rs.dbSession)
+            dao.update(success.toObject, routeIds.last)(rs.dbSession)
+            Ok(s"Entity with id: ${routeIds.last} has been successfully edited.")
+          }
+        )
+      }).getOrElse(BadRequest(s"Cannot edit non-existent entity: ${routeIds.last}"))
+    }
+  }
+
+  def find(routeIds: Id*) = serveHttpResponseWithDB {
+    implicit rs =>
+      dao.find(routeIds)(rs.dbSession).map { entity =>
+        validator.validateGetRequest(entity)(rs.dbSession)
+        Ok(dto.parse(entity).toJson)
+      }.getOrElse(NotFound)
+  }
+
+  def findAll(routeIds: Id*) = serveHttpResponseWithDB {
+    implicit rs =>
+      validator.validateGetAllRequest(rs.dbSession)
+      Ok(JsArray(dao.findAll(routeIds)(rs.dbSession)
+        .map(entity => dto.parse(entity).toJson)))
   }
 }
--- app/controllers/TerritoryController.scala
+++ app/controllers/TerritoryController.scala
@@ -2,29 +2,31 @@ package controllers
 
 import com.typesafe.scalalogging.slf4j.Logger
 import controllers.validator.TerritoryValidator
-import db.dao.{TournamentDao, TerritoryDao, CityDao}
+import db.dao.{CityDao, TerritoryDao, TournamentDao}
 import dto.mapper.TerritoryDtoMapper
+import models.Common._
 import models.territory.Territory
 import models.tournament.{GameRules, TournamentImpl}
 import org.slf4j.LoggerFactory
 import play.api.data.Forms._
 import play.api.data._
 import play.api.db.slick._
+import play.api.libs.json.{JsArray, Json}
 import play.api.mvc.{Action, AnyContent, Result}
 import scaldi.{Injectable, Injector}
 
 import scala.slick.jdbc.JdbcBackend
 
-class TerritoryController(implicit inj: Injector) extends BaseCrudController[Territory] with Injectable {
+class TerritoryController(implicit inj: Injector) extends BaseController with Injectable {
   private implicit val log = Logger(LoggerFactory.getLogger("app"))
 
   private val cityDao = inject[CityDao]
   private val territoryDao = inject[TerritoryDao]
   private val tournamentDao = inject[TournamentDao]
 
-  override protected val validator = inject[TerritoryValidator]
-  override protected val dto = inject[TerritoryDtoMapper]
-  override protected val dao = inject[TerritoryDao]
+  protected val validator = inject[TerritoryValidator]
+  protected val dto = inject[TerritoryDtoMapper]
+  protected val dao = inject[TerritoryDao]
 
   def findByCode(code: String) = serveHttpResponseWithDB { implicit rs =>
     handleOptionalTerritory(territoryDao.find(code))
@@ -80,5 +82,64 @@ class TerritoryController(implicit inj: Injector) extends BaseCrudController[Ter
         validator.validateGetRequest(entity)(rs.dbSession)
         Ok(dto.parse(entity).toJson)
       }.getOrElse(NotFound)
+  }
+
+  def create(routeIds: Id*) = serveHttpResponseWithTransactionalDB {
+    implicit rs => {
+      dto.form.bindFromRequest.fold(
+        hasErrors => {
+          BadRequest("Cannot bind model from request " + hasErrors)
+        },
+        success => {
+          val obj = success.toObject
+          validator.validateCreateRequest(obj)(rs.dbSession)
+          val id = dao.insert(obj)(rs.dbSession)
+
+          Ok(Json.obj("id" -> id))
+        }
+      )
+    }
+  }
+
+  def delete(routeIds: Id*) = serveHttpResponseWithTransactionalDB {
+    implicit rs => {
+      dao.find(routeIds)(rs.dbSession).map(obj => {
+        validator.validateDeleteRequest(obj)(rs.dbSession)
+        dao.delete(obj)(rs.dbSession)
+        Ok(s"Entity [id=${routeIds.last}] has been deleted.")
+      }).getOrElse(BadRequest(s"Cannot delete non-existent entity: [id=${routeIds.last}]"))
+    }
+  }
+
+  def edit(routeIds: Id*) = serveHttpResponseWithTransactionalDB {
+    implicit rs => {
+      dao.find(routeIds)(rs.dbSession).map(obj => {
+        dto.form.bindFromRequest.fold(
+          hasErrors => {
+            BadRequest(s"Cannot bind entity from request $hasErrors")
+          },
+          success => {
+            validator.validateEditRequest(obj, success.toObject)(rs.dbSession)
+            dao.update(success.toObject, routeIds.last)(rs.dbSession)
+            Ok(s"Entity with id: ${routeIds.last} has been successfully edited.")
+          }
+        )
+      }).getOrElse(BadRequest(s"Cannot edit non-existent entity: ${routeIds.last}"))
+    }
+  }
+
+  def find(routeIds: Id*) = serveHttpResponseWithDB {
+    implicit rs =>
+      dao.find(routeIds)(rs.dbSession).map { entity =>
+        validator.validateGetRequest(entity)(rs.dbSession)
+        Ok(dto.parse(entity).toJson)
+      }.getOrElse(NotFound)
+  }
+
+  def findAll(routeIds: Id*) = serveHttpResponseWithDB {
+    implicit rs =>
+      validator.validateGetAllRequest(rs.dbSession)
+      Ok(JsArray(dao.findAll(routeIds)(rs.dbSession)
+        .map(entity => dto.parse(entity).toJson)))
   }
 }
